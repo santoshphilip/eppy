d0d4a4e92e446211816b50e8d81dc8799532334a
santosh <santosh_philip@yahoo.com>
2012-11-29 15:02:38 -0800

    deleted some temporary files

--- geometry/temp/test_foo.py
+++
@@ -1,8 +0,0 @@
-import foo
-from pytest_helpers import almostequal
-
-def test_foo():
-    a = 5
-    answer = 1.667
-    result = foo.div3(a)
-    assert almostequal(answer, result, places=2) == True
