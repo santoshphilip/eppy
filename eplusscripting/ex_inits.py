73501e7c9f2dc021b03195cdf8015be293082dcd
santosh <santosh_philip@yahoo.com>
2013-04-03 17:52:34 -0700

    Geometry functions in documentation

--- eplusscripting/ex_inits.py
+++ eplusscripting/ex_inits.py
@@ -3,3 +3,4 @@
 from IPython.display import Image
 from IPython.display import display_pretty, display_html, display_jpeg, display_png, display_json, display_latex, display_svg
 idfeditor = Image(filename='./documentation/images/idf3.png')
+material_lists = Image(filename='./documentation/images/material_lists.png')
--- eplusscripting/ex_simpleidf.py
+++
@@ -1,9 +0,0 @@
-"""use pyenergyplus to ook at a simple file"""
-
-from idfreader import idfreader
-
-iddfile = "../iddfiles/Energy+V7_2_0.idd"
-fname = "../idffiles/V_7_2/smallfile.idf"
- 
-
-model, to_print, ignorethis = idfreader(fname, iddfile)
