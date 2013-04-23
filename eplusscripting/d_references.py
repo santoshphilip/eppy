f637902d843a40f7b81550eb4de6ccffa325c266
santosh <santosh_philip@yahoo.com>
2013-04-23 09:11:51 -0700

    started on references and rename an object

--- eplusscripting/d_references.py
+++ eplusscripting/d_references.py
@@ -2,21 +2,59 @@
 
 from idfreader import idfreader
 
-iddfile = "../iddfiles/Energy+V7_0_0_036.idd"
-fname = "../idffiles/V_7_0/5ZoneSupRetPlenRAB.idf"
+# iddfile = "../iddfiles/Energy+V7_0_0_036.idd"
+# fname = "../idffiles/V_7_0/5ZoneSupRetPlenRAB.idf"
+iddfile = "../iddfiles/Energy+V7_2_0.idd"
+fname = "../idffiles/V_7_2/constructions.idf"
  
-bunchdt, data, commdct = idfreader(fname, iddfile)
+bunchdt, data, commdct = idfreader(fname, iddfile)  
 
-for i, key in enumerate(data.dtls):
+def getreferences(data, commdct, key):
+    i = data.dtls.index(key)
     for fieldcomm in commdct[i]:
-            # print comm
-            if fieldcomm.has_key('reference'):
-                if fieldcomm['field'] != ["Name"]:
-                    print key
-                    print fieldcomm['field']
-                    print fieldcomm['reference']
-                    print fieldcomm['type']
-                    print fieldcomm.keys()
+        if fieldcomm.has_key('reference'):
+            if fieldcomm['field'][0].endswith('Name'):
+                return fieldcomm['reference']
+                
+def getrefered_inner(data, commdct, refs):
+    refered = {}
+    for i, fieldcomms in enumerate(commdct):
+        for j, fieldcomm in enumerate(fieldcomms):
+            if fieldcomm.has_key('object-list'):
+                theobjectlist = fieldcomm['object-list']
+                refs = [ref.upper() for ref in refs]
+                if theobjectlist[0].upper() in refs:
+                    refered[data.dtls[i]] = j
+    return refered
+    
+def getrefered(data, commdct, key):
+    refs =  getreferences(data, commdct, key)
+    return getrefered_inner(data, commdct, refs)
+    
+refered =  getrefered(data, commdct, 'construction'.upper())
+
+# refered = {}
+# for i, fieldcomms in enumerate(commdct):
+#     for j, fieldcomm in enumerate(fieldcomms):
+#         if fieldcomm.has_key('object-list'):
+#             theobjectlist = fieldcomm['object-list']
+#             refs = [ref.upper() for ref in refs]
+#             if theobjectlist[0].upper() in refs:
+#                 refered[data.dtls[i]] = j
+#                 # print theobjectlist[0], data.dtls[i], j
+for key, item in refered.items():
+    print key, item
+
+
+# for i, key in enumerate(data.dtls):
+#     if key == 'ZONE':
+#         for fieldcomm in commdct[i]:
+#             if fieldcomm.has_key('reference'):
+#                 if fieldcomm['field'][0].endswith('Name'):# != ["Name"]:
+#                     print key
+#                     print fieldcomm['field']
+#                     print fieldcomm['reference']
+#                     print fieldcomm.keys()
         # print key_comm
         # print
         # for comms in key_comm:
@@ -28,4 +66,6 @@ for i, key in enumerate(data.dtls):
         #         print comm['reference']
     # if i > 3:
     #     break
-    
+
+# find reference    
+# (key, name, field)
