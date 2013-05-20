86ebfc08c64f7f1971df0c3083e5fab2a3d6d29f
le0ra <leora@coolshadow.com>
2013-05-20 11:03:32 -0700

    small changes

--- EPlusWebSite/settings.py
+++ EPlusWebSite/settings.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2012 Santosh Phillip
+
+# This file is part of eplusinterface_diagrams.
+
+# Eplusinterface_diagrams is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Eplusinterface_diagrams is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with eplusinterface_diagrams.  If not, see <http://www.gnu.org/licenses/>.
+
 # Django settings for EPlusWebSite project.
 
 DEBUG = True
