bf4b3ac4a6c2682b079aff6dfd73c3f311793d90
le0ra <leora@coolshadow.com>
2013-06-12 12:11:22 -0700

    philip

--- diagrams/iddV6_0.py
+++ diagrams/iddV6_0.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2012 Santosh Philip
+
+# This file is part of eppy.
+
+# Eppy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Eppy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with eppy.  If not, see <http://www.gnu.org/licenses/>.
+
 """iditializes the idd version 6.0
 used in unit testing to save time.
 the idd file will be parsed the first time it is imported"""
