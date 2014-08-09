833ebcf574ad24ab05ba7b4405aad1bf81b2fbc2
santoshphilip <santosh_philip@yahoo.com>
2014-08-09 11:31:59 -0700

    idfdiff2.py works correctly

--- eppy/usefull_scripts/temp.py
+++ eppy/usefull_scripts/temp.py
@@ -1,5 +1,7 @@
 """scripting for idfdiff.py"""
 
+import idfdiff_functions
+
 import sys
 import getopt
 
@@ -16,6 +18,31 @@ from eppy.modeleditor import IDF
 iddfile = "../resources/iddfiles/Energy+V7_2_0.idd"
 fname1 = "../resources/idffiles/V_7_2/box.idf"
 fname2 = "../resources/idffiles/V_7_2/box_diff.idf"
+fname1 = 'f1.idf'
+fname2 = 'f2.idf'
 IDF.setiddname(iddfile)
 idf1 = IDF(fname1)
 idf2 = IDF(fname2)
+
+# - 
+keys = idf1.model.dtls # undocumented variable
+for akey in keys:
+    idfobjs1 = idf1.idfobjects[akey]
+    if idfobjs1:
+        # check for dups
+        names = [idfobj['obj'][1] for idfobj in idfobjs1]
+        print names
+        dups =  idfdiff_functions.find_duplicates(names)
+        print dups
+        print idfobj.keys()
+        print idfobj['objls']
+        print '----'
+        if dups:
+            print "*** Warning ***"
+        for dup in dups:
+            print "There  is more than one %s of %s=%s in first file" % (akey, 
+                        idfobj['objls'][1], dup)
+        if dups:
+            print "The duplicates will be ignored"
+            print '---------'
+
