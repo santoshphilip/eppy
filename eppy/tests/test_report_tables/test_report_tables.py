2ec0928bbb1d11c4a8bf7bbf694bb593a204b48e
Eric Youngson <eric@successionecological.com>
2014-09-07 11:41:50 -0700

    Branch Intent: Create functions to make a set of tables whose data can be referenced by 	EnergyPlus table name, and whose data can be accessed by column & header name Commit Intent: Create a new unit test for the _report_tables revised function before 	commiting to readhtml.py module --------------------------------------------------------------------------------------- *test_report_tables.py -Added new version of _report_talbes function and isolated unit test to build on  prototype, created in IPython, using test driven development practices with pytest...
    *.gitignore
    -Removed generic 'test' files to include new isolated function test,
     'test_report_tables.py'...
    
    *Ran py.test for test_report_tables automated test

---
+++ eppy/tests/test_report_tables/test_report_tables.py
@@ -0,0 +1,69 @@
+from eppy import readhtml
+
+htmlDoc = open('/Users/eayoungs/repo/Sim/Templates/Scripting/Output/ASHRAE90.1_RetailStandalone_STD2010_SalemTable.html', 'r')
+thisLinesTable = readhtml.lines_table(htmlDoc, True)
+
+def _report_tables(linesTable):
+    """Uses the output of lines_table function to produce a set
+    of report tables addressable by name """
+    
+    reportNm = []
+    tableDict = {}
+    reportDict = {}
+    indx = 0
+
+    for i in range(len(linesTable)):
+        reportHeader = (linesTable[i][0])
+        for j in range(len(reportHeader)):
+            if 'Report: ' in reportHeader[j]:
+                reportNm.append(reportHeader[j])
+                if reportNm[indx] != reportNm[indx-1]:
+                    #print(reportNm[indx], reportHeader[2], 'Report')
+                    reportDict[reportHeader[j]] = linesTable[i][1]
+                else: 
+                    #print(reportNm[indx], reportHeader[2], 'Table')
+                indx = indx+1
+        return reportDict.keys()
+
+# def select_table(report_name, table_name, html_doc):
+#    """Uses the output of report_tables function to produce a
+#        set of nested tables where table values can be
+#        addressed by column & row names"""
+#    htmlDoc = open(html_doc, 'r')
+#    htmlContents = htmlDoc.read()
+#    tableDict = report_tables(htmlContents)
+#    grid = tableDict[report_name]
+#    return grid
+
+def test_report_tables():
+    """py.test for test_named_tables"""
+    result = _report_tables(thisLinesTable)
+    assert result == [u'Report: Annual Building Utility Performance Summary',
+ u'Report: Input Verification and Results Summary',
+ u'Report: Climatic Data Summary',
+ u'Report: Envelope Summary',
+ u'Report: Equipment Summary',
+ u'Report: HVAC Sizing Summary',
+ u'Report: System Summary',
+ u'Report: Component Sizing Summary',
+ u'Report: MONTHLY INDOOR TEMP',
+ u'Report: OCCUPANT COMFORT DATA SUMMARY',
+ u'Report: EMISSIONS DATA SUMMARY',
+ u'Report: OUTDOOR AIR SUMMARY',
+ u'Report: VENTILATION LOAD COMPONENTS',
+ u'Report: OVERALL HVAC AIR SYSTEM LOADS',
+ u'Report: OVERALL HVAC SYSTEM ENERGY',
+ u'Report: COMPONENTS OF PEAK ELECTRICAL DEMAND',
+ u'Report: COMPONENTS OF PEAK NET ELECTRICAL DEMAND',
+ u'Report: SETPOINTS NOT MET WITH TEMPERATURES',
+ u'Report: BUILDING LOADS - COOLING',
+ u'Report: BUILDING LOADS - HEATING',
+ u'Report: BUILDING LOADS - ELECTRIC',
+ u'Report: SPACE LOADS',
+ u'Report: ENERGY CONSUMPTION - ELECTRICITY & NATURAL GAS',
+ u'Report: BUILDING ENERGY PERFORMANCE - ELECTRICITY',
+ u'Report: BUILDING ENERGY PERFORMANCE - NATURAL GAS',
+ u'Report: PEAK ENERGY END-USE - ELECTRICITY PART 1',
+ u'Report: PEAK ELECTRICAL DEMAND',
+ u'Report: PEAK GAS DEMAND',
+ u'Report: EndUseEnergyConsumptionElectricityMonthly']
