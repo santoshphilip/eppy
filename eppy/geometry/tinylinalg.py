7290d0afff029950b0630714b4759ab0dae5a683
Eric Youngson <eric@scneco.com>
2015-07-10 17:14:44 -0700

    Revised header and doc-string for tinylinalg module

--- eppy/geometry/tinylinalg.py
+++ eppy/geometry/tinylinalg.py
@@ -1,26 +1,9 @@
 # Copyright (c) 2014 Eric Allen Youngson
 
-# This file is part of eppy.
-
-# Eppy is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Eppy is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with eppy.  If not, see <http://www.gnu.org/licenses/>.
-
 """This module is used to implement native Python functions to replace those
-    called from numpy, when using eppy with Rhino"""
+    called from numpy, when not available"""
 # Written by Eric Youngson eric@scneco.com / eayoungs@gmail.com
 # Succession Ecological Services: Portland, Oregon
-
-import tinynumpy as tnp
 
 class LinAlgError(Exception):
     pass
