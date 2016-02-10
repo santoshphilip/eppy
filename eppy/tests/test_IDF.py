166ff1cceab002f249b18383af3f2d85a9d8bb3c
Jamie Bull <jamie.bull@oco-carbon.com>
2016-02-10 01:02:14 +0100

    Fix IDF tests

--- eppy/tests/test_IDF.py
+++ eppy/tests/test_IDF.py
@@ -7,43 +7,40 @@
 
 """py.test for class IDF"""
 
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
-
-from eppy.modeleditor import IDF0
-
-def test_IDF0():
-    """py.test for class IDF0"""
-    assert IDF0.iddname == None
-    IDF0.setiddname("gumby", testing=True)
-    assert IDF0.iddname == "gumby"
-    IDF0.setiddname("karamba", testing=True)
-    assert IDF0.iddname != "karamba"
-    assert IDF0.iddname == "gumby"
-
-
-# import eppy.snippet as snippet
-
-
-from eppy.iddcurrent import iddcurrent
-iddsnippet = iddcurrent.iddtxt
-
-
 from StringIO import StringIO
+from eppy.iddcurrent import iddcurrent
+from eppy.modeleditor import IDF
+
+
+def test_IDF():
+    """py.test for class IDF"""
+    stored_idd = IDF.iddname
+    IDF.iddname = None
+    assert IDF.iddname == None
+    IDF.setiddname("gumby", testing=True)
+    assert IDF.iddname == "gumby"
+    IDF.setiddname("karamba", testing=True)
+    assert IDF.iddname != "karamba"
+    assert IDF.iddname == "gumby"
+    IDF.iddname = stored_idd
+
+iddsnippet = iddcurrent.iddtxt
 iddfhandle = StringIO(iddsnippet)
 
-from eppy.modeleditor import IDF
 
 if IDF.getiddname() == None:
     IDF.setiddname(iddfhandle)
 
+
 class TestIDF(object):
+
     """py.test for IDF function"""
+
     def test_removeidfobject(self):
         """py.test for IDF.removeidfobject """
         idftxt = ""
@@ -74,4 +71,3 @@ class TestIDF(object):
         idf.popidfobject(key, 2)
         assert buildings[2].Name == "Building2"
         assert idf.model.dt[key][2][1] == "Building2"
-        
