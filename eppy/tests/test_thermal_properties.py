2a251106446c9ee61daf3081a753db77e4b73c22
Jamie Bull <jamie.bull@oco-carbon.com>
2016-06-23 17:23:25 +0200

    Fix formatting

--- eppy/tests/test_thermal_properties.py
+++ eppy/tests/test_thermal_properties.py
@@ -1,12 +1,11 @@
-#==============================================================================
-# # Copyright (c) 2016 Jamie Bull
-# # =======================================================================
-# #  Distributed under the MIT License.
-# #  (See accompanying file LICENSE or copy at
-# #  http://opensource.org/licenses/MIT)
-# # =======================================================================
-# """py.test for thermal_properties.py"""
-#  
+# Copyright (c) 2016 Jamie Bull
+# =======================================================================
+#  Distributed under the MIT License.
+#  (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+# =======================================================================
+"""py.test for thermal_properties.py"""
+  
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
