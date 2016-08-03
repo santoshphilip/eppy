1db2dbf9a522ce3f232d662408ed4066a78318d5
Jamie Bull <jamie.bull@oco-carbon.com>
2016-08-03 19:50:20 +0200

    Tests passing using idd_index
    Improved docs and test structure. Added support for infrared transparent
    materials.

--- eppy/constructions/thermal_properties.py
+++ eppy/constructions/thermal_properties.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2016 Jamie Bull
 # =======================================================================
 #  Distributed under the MIT License.
@@ -6,7 +7,6 @@
 # =======================================================================
 """Functions to calculate the thermal properties of constructions and materials.
 """
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -23,22 +23,27 @@ def rvalue(ddtt):
     R value (W/K) of a construction or material.
     thickness (m) / conductivity (W/m-K)
     """
-    if ddtt.obj[0] == 'Material':
-        thickness = ddtt.obj[ddtt.objls.index('Thickness')]
-        conductivity = ddtt.obj[ddtt.objls.index('Conductivity')]
-        rvalue = thickness / conductivity
-    elif ddtt.obj[0] == 'Material:AirGap':
-        rvalue = ddtt.obj[ddtt.objls.index('Thermal_Resistance')] 
-    elif ddtt.obj[0] == 'Construction':
+    object_type = ddtt.obj[0]
+    if object_type == 'Construction':
         rvalue = INSIDE_FILM_R + OUTSIDE_FILM_R
         layers = ddtt.obj[2:]
-        # TODO: function and data structure for getting object-lists
-        object_list = ['MATERIAL', 'MATERIAL:AIRGAP']
-        for key, layer in product(object_list, layers):
+        field_idd = ddtt.getfieldidd('Outside_Layer')
+        validobjects = field_idd['validobjects']
+        for key, layer in product(validobjects, layers):
             try:
                 rvalue += ddtt.theidf.getobject(key, layer).rvalue
             except AttributeError:
                 pass
+    elif object_type == 'Material':
+        thickness = ddtt.obj[ddtt.objls.index('Thickness')]
+        conductivity = ddtt.obj[ddtt.objls.index('Conductivity')]
+        rvalue = thickness / conductivity
+    elif object_type == 'Material:AirGap':
+        rvalue = ddtt.obj[ddtt.objls.index('Thermal_Resistance')] 
+    elif object_type == 'Material:InfraredTransparent':
+        rvalue = 0
+    else:
+        raise AttributeError("%s has no rvalue property" % object_type)
     return rvalue
 
 def ufactor(ddtt):
@@ -49,7 +54,10 @@ def ufactor(ddtt):
     return 1 / rvalue(ddtt)
     
 def ufactor_ip(ddtt):
-    """return ufactor in IP inits"""
+    """
+    U factor (BTU/(h °F ft^2)) of a construction or material.
+    1 / R value (ft^2 °F hr/Btu)
+    """
     # quick fix for Santosh. Needs to thought thru
     mult = 0.076 / 0.429 # base on doing conversion in the table report
     return ufactor(ddtt) * mult
@@ -57,28 +65,34 @@ def ufactor_ip(ddtt):
 def rvalue_ip(ddtt):
     """return R value in IP units"""
     # quick fix for Santosh. Needs to thought thru
-    return 1. /  ufactor_ip(ddtt)       
+    return 1 /  ufactor_ip(ddtt)       
 
 def heatcapacity(ddtt):
     """
     Heat capacity (kJ/m2-K) of a construction or material.
     thickness (m) * density (kg/m3) * specific heat (J/kg-K) * 0.001
     """
-    if ddtt.obj[0] == 'Material':
-        thickness = ddtt.obj[ddtt.objls.index('Thickness')]
-        density = ddtt.obj[ddtt.objls.index('Density')]
-        specificheat = ddtt.obj[ddtt.objls.index('Specific_Heat')]
-        heatcapacity = thickness * density * specificheat * 0.001
-    elif ddtt.obj[0] == 'Material:AirGap':
-        heatcapacity = 0
-    elif ddtt.obj[0] == 'Construction':
+    object_type = ddtt.obj[0]
+    if object_type == 'Construction':
         heatcapacity = 0
         layers = ddtt.obj[2:]
-        object_list = ['MATERIAL', 'MATERIAL:AIRGAP']
-        for key, layer in product(object_list, layers):
+        field_idd = ddtt.getfieldidd('Outside_Layer')
+        validobjects = field_idd['validobjects']
+        for key, layer in product(validobjects, layers):
             try:
                 heatcapacity += ddtt.theidf.getobject(key, layer).heatcapacity
             except AttributeError:
                 pass
+    elif object_type == 'Material':
+        thickness = ddtt.obj[ddtt.objls.index('Thickness')]
+        density = ddtt.obj[ddtt.objls.index('Density')]
+        specificheat = ddtt.obj[ddtt.objls.index('Specific_Heat')]
+        heatcapacity = thickness * density * specificheat * 0.001
+    elif object_type == 'Material:AirGap':
+        heatcapacity = 0
+    elif object_type == 'Material:InfraredTransparent':
+        heatcapacity = 0
+    else:
+        raise AttributeError("%s has no heatcapacity property" % object_type)
     return heatcapacity
         
--- eppy/tests/test_thermal_properties.py
+++ eppy/tests/test_thermal_properties.py
@@ -8,18 +8,18 @@
 """
 from six import StringIO
 
-from eppy.iddcurrent import iddcurrent
-from eppy.modeleditor import IDF
-
 from eppy.constructions.thermal_properties import INSIDE_FILM_R
 from eppy.constructions.thermal_properties import OUTSIDE_FILM_R
+from eppy.iddcurrent import iddcurrent
+from eppy.modeleditor import IDF
 from eppy.pytest_helpers import almostequal
 
 
 iddfhandle = StringIO(iddcurrent.iddtxt)
+  
 if IDF.getiddname() == None:
     IDF.setiddname(iddfhandle)
-  
+
 single_layer = """
   Construction,
     TestConstruction,                       !- Name
@@ -77,38 +77,58 @@ air_gap = """
     0.1;                     !- Thermal Resistance
     """
   
-
-class TestThermalProperties(object):
-      
+infrared_transparent = """
+  Construction,
+    TestConstruction,        !- Name
+    TestMaterial,            !- Inside Layer
+    InfraredTransparent,     !- Layer 2
+    TestMaterial;            !- Outside Layer
+    
+  Material,
+    TestMaterial,
+    Rough,                   !- Roughness
+    0.10,                    !- Thickness {m}
+    0.5,                     !- Conductivity {W/m-K}
+    1000.0,                  !- Density {kg/m3}
+    1200,                    !- Specific Heat {J/kg-K}
+    0.9,                     !- Thermal Absorptance
+    0.6,                     !- Solar Absorptance
+    0.6;                     !- Visible Absorptance
+  Material:InfraredTransparent,
+    InfraredTransparent;     !- Name
+    """
+  
+class Test_ThermalProperties(object):
+    
+    def setup_method(self, test_method):
+        self.idf = IDF()
+        
     def test_rvalue_1_layer_construction(self):
-        idf = IDF()
-        idf.initreadtxt(single_layer)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(single_layer)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = (INSIDE_FILM_R +
                     m.Thickness / m.Conductivity +
                     OUTSIDE_FILM_R)
         assert c.rvalue == expected
         assert c.rvalue == 0.35
-
+    
     def test_rvalue_2_layer_construction(self):
-        idf = IDF()
-        idf.initreadtxt(double_layer)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(double_layer)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = (INSIDE_FILM_R +
                     m.Thickness / m.Conductivity +
                     m.Thickness / m.Conductivity +
                     OUTSIDE_FILM_R)
         assert c.rvalue == expected
         assert c.rvalue == 0.55
-
+    
     def test_rvalue_airgap_construction(self):
-        idf = IDF()
-        idf.initreadtxt(air_gap)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
-        a = idf.getobject('MATERIAL:AIRGAP', 'AirGap')
+        self.idf.initreadtxt(air_gap)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
+        a = self.idf.getobject('MATERIAL:AIRGAP', 'AirGap')
         expected = (INSIDE_FILM_R +
                     m.Thickness / m.Conductivity +
                     a.Thermal_Resistance +
@@ -116,20 +136,29 @@ class TestThermalProperties(object):
                     OUTSIDE_FILM_R)
         assert almostequal(c.rvalue, expected, places=2)
         assert almostequal(c.rvalue, 0.65, places=2)
-
+    
+    def test_rvalue_infraredtransparent_construction(self):
+        self.idf.initreadtxt(infrared_transparent)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
+        expected = (INSIDE_FILM_R +
+                    m.Thickness / m.Conductivity +
+                    m.Thickness / m.Conductivity +
+                    OUTSIDE_FILM_R)
+        assert almostequal(c.rvalue, expected, places=2)
+        assert almostequal(c.rvalue, 0.55, places=2)
+    
     def test_rvalue_material(self):
-        idf = IDF()
-        idf.initreadtxt(single_layer)
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(single_layer)
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = m.Thickness / m.Conductivity
         assert m.rvalue == expected
         assert m.rvalue == 0.2
           
     def test_ufactor_1_layer_construction(self):
-        idf = IDF()
-        idf.initreadtxt(single_layer)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(single_layer)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = 1 / (INSIDE_FILM_R +
                         m.Thickness / m.Conductivity + 
                         OUTSIDE_FILM_R)
@@ -137,10 +166,9 @@ class TestThermalProperties(object):
         assert c.ufactor == 1 / 0.35
       
     def test_ufactor_2_layer_construction(self):
-        idf = IDF()
-        idf.initreadtxt(double_layer)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(double_layer)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = 1 / (INSIDE_FILM_R +
                         m.Thickness / m.Conductivity + 
                         m.Thickness / m.Conductivity + 
@@ -149,11 +177,10 @@ class TestThermalProperties(object):
         assert c.ufactor == 1 / 0.55
       
     def test_ufactor_airgap_construction(self):
-        idf = IDF()
-        idf.initreadtxt(air_gap)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
-        a = idf.getobject('MATERIAL:AIRGAP', 'AirGap')
+        self.idf.initreadtxt(air_gap)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
+        a = self.idf.getobject('MATERIAL:AIRGAP', 'AirGap')
         expected = 1 /(INSIDE_FILM_R +
                        m.Thickness / m.Conductivity +
                        a.Thermal_Resistance +
@@ -161,47 +188,60 @@ class TestThermalProperties(object):
                        OUTSIDE_FILM_R)
         assert almostequal(c.ufactor, expected, places=2)
         assert almostequal(c.ufactor, 1 / 0.65, places=2)
-
+    
+    def test_ufactor_infraredtransparent_construction(self):
+        self.idf.initreadtxt(infrared_transparent)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
+        expected = 1 /(INSIDE_FILM_R +
+                       m.Thickness / m.Conductivity +
+                       m.Thickness / m.Conductivity +
+                       OUTSIDE_FILM_R)
+        assert almostequal(c.ufactor, expected, places=2)
+        assert almostequal(c.ufactor, 1 / 0.55, places=2)
+    
     def test_ufactor_material(self):
-        idf = IDF()
-        idf.initreadtxt(single_layer)
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(single_layer)
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = 1 / (m.Thickness / m.Conductivity)
         assert m.ufactor == expected
         assert m.ufactor == 1 / 0.2
               
     def test_heatcapacity_1_layer_construction(self):
-        idf = IDF()
-        idf.initreadtxt(single_layer)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(single_layer)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = m.Thickness * m.Specific_Heat * m.Density * 0.001
         assert c.heatcapacity == expected
         assert c.heatcapacity == 120
       
     def test_heatcapacity_2_layer_construction(self):
-        idf = IDF()
-        idf.initreadtxt(double_layer)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(double_layer)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = m.Thickness * m.Specific_Heat * m.Density * 0.001 * 2
         assert c.heatcapacity == expected
         assert c.heatcapacity == 240
       
     def test_heatcapacity_airgap_construction(self):
-        idf = IDF()
-        idf.initreadtxt(air_gap)
-        c = idf.getobject('CONSTRUCTION', 'TestConstruction')
-        m = idf.getobject('MATERIAL', 'TestMaterial')
-        a = idf.getobject('MATERIAL:AIRGAP', 'AirGap')
+        self.idf.initreadtxt(air_gap)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = m.Thickness * m.Specific_Heat * m.Density * 0.001 * 2
         assert almostequal(c.heatcapacity, expected, places=2)
         assert almostequal(c.heatcapacity, 240, places=2)
-
+    
+    def test_heatcapacity_infraredtransparent_construction(self):
+        self.idf.initreadtxt(infrared_transparent)
+        c = self.idf.getobject('CONSTRUCTION', 'TestConstruction')
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
+        expected = m.Thickness * m.Specific_Heat * m.Density * 0.001 * 2
+        assert almostequal(c.heatcapacity, expected, places=2)
+        assert almostequal(c.heatcapacity, 240, places=2)
+    
     def test_heatcapacity_material(self):
-        idf = IDF()
-        idf.initreadtxt(single_layer)
-        m = idf.getobject('MATERIAL', 'TestMaterial')
+        self.idf.initreadtxt(single_layer)
+        m = self.idf.getobject('MATERIAL', 'TestMaterial')
         expected = (m.Thickness * m.Specific_Heat * m.Density * 0.001)
         assert m.heatcapacity == expected
         assert m.heatcapacity == 120
