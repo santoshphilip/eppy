d359dd5b8dde15f6f495f0fb35bdc34a06d0e371
santoshphilip <santosh_philip@yahoo.com>
2016-09-04 19:41:31 -0700

    removed build/ eppy.egg-info/ and /dist

--- build/lib/eppy/iddcurrent.py
+++
@@ -1,8 +0,0 @@
-"""current idd"""
-# =======================================================================
-#  Distributed under the MIT License.
-#  (See accompanying file LICENSE or copy at
-#  http://opensource.org/licenses/MIT)
-# =======================================================================
-import eppy.iddv800 as iddcurrent
-# import iddv7 as iddcurrent
