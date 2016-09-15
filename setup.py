e6340c499664b673c320030bca03bd32812d7c52
Jamie Bull <jamie.bull@oco-carbon.com>
2016-09-15 08:21:23 +0200

    Improve pytest integration and setup.py for CI platforms
    Remove pytest from install_requires.
    Remove unused sections of setup.py.
    Call pytest using `python setup.py test` on CI platforms.
    Create the long description directly from README.md
    Avoid deprecated [pytest] option
    Omit venv from test coverage report.
    Upgrade setuptools to avoid failing CI on OSX.
    Fix coverage reporting on Appveyor.

--- setup.py
+++ setup.py
@@ -1,36 +1,28 @@
 
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import io
-import codecs
 import os
-import sys
 
 import eppy
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-long_description = read('README.txt')
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
+def read_md(f):
+    try:
+        from pypandoc import convert
+        try:
+            return convert(os.path.join(THIS_DIR, f), 'rst')
+        except:
+            return "Eppy"
+    except ImportError:
+        print("warning: pypandoc module not found, could not convert Markdown to RST")
+        try:
+            with open(os.path.join(THIS_DIR, f), 'r') as f_in:
+                return f_in.read()
+        except:
+            return "Eppy"
+    
 
 setup(
     name='eppy',
@@ -38,19 +30,15 @@ setup(
     url='https://github.com/santoshphilip/eppy',
     license='MIT License',
     author='Santosh Philip',
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
     author_email='eppy_scripting@yahoo.com',
     description='Scripting language for E+ idf files, and E+ output files',
-    long_description=long_description,# TODO set this up
+    long_description=read_md('README.md'),
     packages=['eppy', 'eppy.EPlusInterfaceFunctions', 'eppy.geometry', 'eppy.constructions'],
     include_package_data=True,
     platforms='any',
-    test_suite='eppy.test.test_eppy',# TODO make test_eppy
     install_requires = [
         "munch>=2.0.2",
         "beautifulsoup4>=4.2.1",
-        "pytest>=2.3.5",
         "tinynumpy>=1.2.1",
         "six>=1.10.0",
         "decorator>=4.0.10"
@@ -67,13 +55,14 @@ setup(
         'Topic :: Scientific/Engineering',
         ],
     extras_require={
-        'python_version<="2.7"': [
+        ':python_version<="2.7"': [
             'pydot>1.0',
-            'pyparsing==1.5.7'
+            'pyparsing>=2.1.4'
             ],
-        'python_version>="3.5"': [
+        ':python_version>="3.5"': [
             'pydot3k',
             ],
-        'testing': ['pytest'],        
-    }
+        },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
