d081a0cb6bb94c8249e1557212731fdd05214881
santoshphilip <santosh_philip@yahoo.com>
2017-08-12 23:02:22 -0700

    tdbr2EOL() converts for <br> to `\n`

--- eppy/results/readhtml.py
+++ eppy/results/readhtml.py
@@ -19,14 +19,25 @@ class NotSimpleTable(Exception):
     """Exception Object"""
     pass
 
-
+def tdbr2EOL(td):
+    """convert the <br/> in <td> block into line ending (EOL = \n)"""
+    for br in td.find_all("br"):
+        br.replace_with("\n")
+    txt = unicode(td) # make it back into test
+    soup = BeautifulSoup(txt, 'lxml') # read it as a BeautifulSoup
+    ntxt = soup.find('td') # BeautifulSoup has lot of other html junk.
+                           # this line will extract just the <td> block 
+    return ntxt
+    
 def is_simpletable(table):
     """test if the table has only strings in the cells"""
     tds = table('td')
     for td in tds:
         if td.contents != []:
+            td = tdbr2EOL(td)
             if len(td.contents) == 1:
-                if not isinstance(td.contents[0], NavigableString):
+                thecontents = td.contents[0]
+                if not isinstance(thecontents, NavigableString):
                     return False
             else:
                 return False
@@ -41,6 +52,7 @@ def table2matrix(table):
     for tr in table('tr'):
         row = []
         for td in tr('td'):
+            td = tdbr2EOL(td) # convert any '<br>' in the td to line ending
             try:
                 row.append(td.contents[0])
             except IndexError:
@@ -57,6 +69,7 @@ def table2val_matrix(table):
     for tr in table('tr'):
         row = []
         for td in tr('td'):
+            td = tdbr2EOL(td)
             try:
                 val = td.contents[0]
             except IndexError:
