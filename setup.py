c4bdeff5987b510519b14292d66d18d385f31317
Santosh Philip <santosh_philip@yahoo.com>
2019-05-23 11:13:09 -0700

    changed version number in setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PyTest(TestCommand):
 
 setup(
     name='eppy',
-    version='0.5.48',
+    version='0.5.49',
     url='https://github.com/santoshphilip/eppy',
     license='MIT License',
     author='Santosh Philip',
