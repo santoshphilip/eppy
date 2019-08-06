a0d2ef50797bd83e9835809d4c2daaca1cdfdb9f
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2019-08-06 14:11:29 -0400

    Revert "Adds expandobject=True to testrunners to assert tests"
    This reverts commit fd8789cd8d807bb94b939bbeb20462feb7cd71fe.

--- eppy/tests/test_runner.py
+++ eppy/tests/test_runner.py
@@ -223,65 +223,33 @@ class TestIDFRunner(object):
             "eplustbl.htm",
             "sqlite.err",
         ]
-        self.expected_files_prefix = [
-            "sqlite.err",
-            "testout.audit",
-            "testout.bnd",
-            "testout.eio",
-            "testout.end",
-            "testout.err",
-            "testout.eso",
-            "testout.expidf",
-            "testout.mdd",
-            "testout.mtd",
-            "testout.rdd",
-            "testout.shd",
-            "testtbl.htm",
-        ]
         self.expected_files_suffix_C = [
             "eplus.audit",
-            "eplus.bnd",
-            "eplus.eio",
-            "eplus.end",
-            "eplus.err",
-            "eplus.eso",
-            "eplus.expidf",
             "eplus.mdd",
-            "eplus.mtd",
-            "eplus.rdd",
-            "eplus.shd",
+            "eplus.err",
             "eplusSqlite.err",
+            "eplus.eio",
             "eplusTable.htm",
+            "eplus.shd",
+            "eplus.mtd",
+            "eplus.bnd",
+            "eplus.eso",
+            "eplus.rdd",
+            "eplus.end",
         ]
         self.expected_files_suffix_D = [
+            "eplus.audit",
+            "eplus.mdd",
             "eplus-sqlite.err",
             "eplus-table.htm",
-            "eplus.audit",
-            "eplus.bnd",
-            "eplus.eio",
-            "eplus.end",
             "eplus.err",
-            "eplus.eso",
-            "eplus.expidf",
-            "eplus.mdd",
-            "eplus.mtd",
-            "eplus.rdd",
+            "eplus.eio",
+            "eplus.bnd",
             "eplus.shd",
-        ]
-        self.expected_files_suffix_L = [
-            "eplusout.audit",
-            "eplusout.bnd",
-            "eplusout.eio",
-            "eplusout.end",
-            "eplusout.err",
-            "eplusout.eso",
-            "eplusout.expidf",
-            "eplusout.mdd",
-            "eplusout.mtd",
-            "eplusout.rdd",
-            "eplusout.shd",
-            "eplustbl.htm",
-            "sqlite.err",
+            "eplus.mtd",
+            "eplus.end",
+            "eplus.eso",
+            "eplus.rdd",
         ]
 
     def teardown(self):
@@ -334,12 +302,7 @@ class TestIDFRunner(object):
 
         """
         test_idf.idfobjects["RUNPERIOD"][0].End_Month = 1
-        test_idf.run(
-            annual=True,
-            readvars=True,
-            output_directory="run_outputs",
-            expandobjects=True,
-        )
+        test_idf.run(annual=True, readvars=True, output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
         self.expected_files.extend(["eplusout.rvaudit", "eplusout.csv"])
@@ -431,12 +394,11 @@ class TestIDFRunner(object):
         Fails on severe errors or unexpected/missing output files.
 
         """
-        test_idf.run(
-            output_prefix="test", output_directory="run_outputs", expandobjects=True
-        )
+        test_idf.run(output_prefix="test", output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
-        assert set(files) == set(self.expected_files_prefix)
+        prefixed_files = [f.replace("eplus", "test") for f in self.expected_files]
+        assert set(files) == set(prefixed_files)
 
     def test_run_output_suffix_L(self, test_idf):
         """
@@ -444,12 +406,10 @@ class TestIDFRunner(object):
         Fails on severe errors or unexpected/missing output files.
 
         """
-        test_idf.run(
-            output_suffix="L", output_directory="run_outputs", expandobjects=True
-        )
+        test_idf.run(output_suffix="L", output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
-        assert set(files) == set(self.expected_files_suffix_L)
+        assert set(files) == set(self.expected_files)
 
     def test_run_output_suffix_C(self, test_idf):
         """
@@ -457,9 +417,7 @@ class TestIDFRunner(object):
         Fails on severe errors or unexpected/missing output files.
 
         """
-        test_idf.run(
-            output_suffix="C", output_directory="run_outputs", expandobjects=True
-        )
+        test_idf.run(output_suffix="C", output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
         assert set(files) == set(self.expected_files_suffix_C)
@@ -470,9 +428,7 @@ class TestIDFRunner(object):
         Fails on severe errors or unexpected/missing output files.
 
         """
-        test_idf.run(
-            output_suffix="D", output_directory="run_outputs", expandobjects=True
-        )
+        test_idf.run(output_suffix="D", output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
         assert set(files) == set(self.expected_files_suffix_D)
@@ -528,10 +484,10 @@ class TestIDFRunner(object):
         Fails if no output received from EnergyPlus.
 
         """
-        test_idf.run(verbose="v", output_directory="run_outputs", expandobjects=True)
+        test_idf.run(verbose="v", output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
-        self.expected_files.extend(["eplusout.expidf"])
+        self.expected_files.extend([])
         assert set(files) == set(self.expected_files)
         out, _err = capfd.readouterr()
         assert len(out) > 0
@@ -543,10 +499,10 @@ class TestIDFRunner(object):
         Fails if output received from EnergyPlus.
 
         """
-        test_idf.run(verbose="q", output_directory="run_outputs", expandobjects=True)
+        test_idf.run(verbose="q", output_directory="run_outputs")
         assert not has_severe_errors()
         files = os.listdir("run_outputs")
-        self.expected_files.extend(["eplusout.expidf"])
+        self.expected_files.extend([])
         assert set(files) == set(self.expected_files)
         out, _err = capfd.readouterr()
         assert len(out) == 0
