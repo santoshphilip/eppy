50c4c63511d079e510e97e5456cbf9e3a22e7b24
santoshphilip <santosh_philip@yahoo.com>
2019-10-13 15:13:32 -0700

    for release r0.5.52

--- eppy/__init__.py
+++ eppy/__init__.py
@@ -15,7 +15,7 @@ from __future__ import unicode_literals
 
 __author__ = """Santosh Philip"""
 __email__ = "santosh@noemail.com"
-__version__ = "0.5.51"
+__version__ = "0.5.52"
 
 
 from six import StringIO
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name="eppy",
-    version="0.5.51",
+    version="0.5.52",
     url="https://github.com/santoshphilip/eppy",
     license="MIT License",
     author="Santosh Philip",
