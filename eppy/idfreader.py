96852ffebee7f667e808f9ab68a0a3eaec4df0cc
Dimitris Mantas <dimitris.mantas@outlook.com>
2021-11-05 04:26:23 +0200

    Issue #360 - Update 2
    Conditionally closes the .IDD file if a TypeError hasn't been previously raised.

--- eppy/idfreader.py
+++ eppy/idfreader.py
@@ -40,6 +40,8 @@ def iddversiontuple(afile):
     except TypeError:
         fhandle = afile
     line1 = fhandle.readline()
+    if fhandle != afile:
+        fhandle.close()
     try:
         line1 = line1.decode("ISO-8859-2")
     except AttributeError:
