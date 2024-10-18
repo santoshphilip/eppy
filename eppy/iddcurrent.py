856f5e09825ddd7f938a8995781e5ce1d642f20a
Santosh Philip <santosh@noemail.com>
2024-10-18 15:01:35 -0700

    working on issue #444
    updated the pytest IDD version to 9.4.0
        modified:   eppy/iddcurrent.py
        new file:   eppy/iddv940.py

--- eppy/iddcurrent.py
+++ eppy/iddcurrent.py
@@ -9,6 +9,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import eppy.iddv800 as iddcurrent
+import eppy.iddv940 as iddcurrent
 
 # import iddv7 as iddcurrent
