d44a359710456f8816b6d1e4db32506cc216122a
Santosh Philip <santosh@noemail.com>
2024-10-19 10:47:23 -0700

    Bump version: 0.5.64 → 0.5.65

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name="eppy",
-    version="0.5.64",
+    version="0.5.65",
     url="https://github.com/santoshphilip/eppy",
     license="MIT License",
     author="Santosh Philip",
